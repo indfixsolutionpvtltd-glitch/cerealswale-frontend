b1576c2f3a2d6cb65db20079f41c9caeb1000a28
indfixsolutionpvtltd-glitch <indfixsolution.pvt.ltd@gmail.com>
2026-05-08 18:54:10 +0530

    firebase.js

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage"; // Naya import
 
 const firebaseConfig = {
   apiKey: "AIzaSyBRc1nuSgPhGJclWVgmcEE8qOrx1ztoS3k",
   authDomain: "cerealswale.firebaseapp.com",
   projectId: "cerealswale",
   storageBucket: "cerealswale.firebasestorage.app",
-  messagingSenderId: "269234122223", // Maine ise theek kar diya hai
+  messagingSenderId: "269234122223",
   appId: "1:269234122223:web:c758a2e39af5ceb2dbbaca",
-  measurementId: "G-JSJP5TBBPG"
 };
 
-// Initialize Firebase (Next.js Friendly Version)
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const storage = getStorage(app); // Storage initialize kiya
 
-export { db };
+export { db, storage };
