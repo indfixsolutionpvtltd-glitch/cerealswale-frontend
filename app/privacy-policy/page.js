64669f864efd9d23bbed2404f23f93057d12bcd5
indfixsolutionpvtltd-glitch <indfixsolution.pvt.ltd@gmail.com>
2026-05-13 16:58:58 +0530

    page.js

--- app/privacy-policy/page.js
+++ app/privacy-policy/page.js
@@ -69,24 +69,12 @@ export default function PrivacyPolicy() {
         It is <strong>CATALYST SERVICE PRIVATE LIMITED</strong>'s policy to
         respect your privacy regarding any information we may collect while
         operating our website. This Privacy Policy applies to{' '}
-        <a href="https://www.cerealswale.com/" style={{ color: '#2e7d32' }}>
-          https://www.cerealswale.com/
+        <a href="[https://www.cerealswale.com/](https://www.cerealswale.com/)" style={{ color: '#2e7d32' }}>
+          [https://www.cerealswale.com/](https://www.cerealswale.com/)
         </a>{' '}
-        (hereinafter, "us", "we", or "https://www.cerealswale.com/"). We respect
+        (hereinafter, "us", "we", or "[https://www.cerealswale.com/](https://www.cerealswale.com/)"). We respect
         your privacy and are committed to protecting personally identifiable
-        information you may provide us through the Website. We have adopted this
-        privacy policy ("Privacy Policy") to explain what information may be
-        collected on our Website, how we use this information, and under what
-        circumstances we may disclose the information to third parties. This
-        Privacy Policy applies only to information we collect through the Website
-        and does not apply to our collection of information from other sources.
-      </p>
-
-      <p>
-        This Privacy Policy, together with the Terms and Conditions posted on our
-        Website, set forth the general rules and policies governing your use of
-        our Website. Depending on your activities when visiting our Website, you
-        may be required to agree to additional terms and conditions.
+        information you may provide us through the Website.
       </p>
 
       <h3 style={{ color: '#2e7d32' }}>Website Visitors</h3>
@@ -95,169 +83,39 @@ export default function PrivacyPolicy() {
         collects non-personally-identifying information of the sort that web
         browsers and servers typically make available, such as the browser type,
         language preference, referring site, and the date and time of each
-        visitor request. Our purpose in collecting non-personally identifying
-        information is to better understand how visitors use the website. From
-        time to time, we may release non-personally-identifying information in
-        the aggregate, e.g., by publishing a report on trends in the usage of its
-        website.
-      </p>
-
-      <p>
-        <strong>CATALYST SERVICE PRIVATE LIMITED</strong> also collects
-        potentially personally-identifying information like Internet Protocol
-        (IP) addresses for logged-in users and for users leaving comments on{' '}
-        <a href="https://www.cerealswale.com/" style={{ color: '#2e7d32' }}>
-          https://www.cerealswale.com/
-        </a>{' '}
-        blog posts. We only disclose logged-in user and commenter IP addresses
-        under the same circumstances that we use and disclose
-        personally-identifying information as described below.
+        visitor request.
       </p>
 
       <h3 style={{ color: '#2e7d32' }}>Sign Up For Our Service</h3>
       <p>
         If you sign up for our service, we will ask you for your name and phone
-        number. The purpose of the processing is to allow you access to our
-        services and to provide the services to you. The purpose is additionally
-        to reach you with important notifications and updates regarding the
-        service.
-      </p>
-
-      <h3 style={{ color: '#2e7d32' }}>
-        Gathering Of Personally-Identifying Information
-      </h3>
-      <p>
-        Certain visitors to our websites choose to interact with us in ways that
-        require <strong>CATALYST SERVICE PRIVATE LIMITED</strong> to gather
-        personally-identifying information. The amount and type of information
-        gathered depends on the nature of the interaction. For example, we ask
-        visitors who sign up for a blog at{' '}
-        <a href="https://www.cerealswale.com/" style={{ color: '#2e7d32' }}>
-          https://www.cerealswale.com/
-        </a>{' '}
-        to provide a username, email address, and phone number.
+        number.
       </p>
 
       <h3 style={{ color: '#2e7d32' }}>Security</h3>
       <p>
         The security of your Personal Information is important to us, but
         remember that no method of transmission over the Internet, or method of
-        electronic storage is 100% secure. While we strive to use commercially
-        acceptable means to protect your Personal Information, we cannot
-        guarantee its absolute security.
-      </p>
-
-      <h3 style={{ color: '#2e7d32' }}>Advertisements</h3>
-      <p>
-        Ads appearing on our website may be delivered to users by advertising
-        partners, who may set cookies. These cookies allow the ad server to
-        recognize your computer each time they send you an online advertisement
-        to compile information about you or others who use your computer. This
-        information allows ad networks to, among other things, deliver targeted
-        advertisements that they believe will be of most interest to you. This
-        Privacy Policy covers the use of cookies by{' '}
-        <strong>CATALYST SERVICE PRIVATE LIMITED</strong> and does not cover the
-        use of cookies by any advertisers.
+        electronic storage is 100% secure.
       </p>
 
       <h3 style={{ color: '#2e7d32' }}>Links To External Sites</h3>
       <p>
         Our Service may contain links to external sites that are not operated by
-        us. If you click on a third-party link, you will be directed to that
-        third party's site. We strongly advise you to review the Privacy Policy
-        and terms and conditions of every site you visit. We have no control
-        over, and assume no responsibility for the content, privacy policies, or
-        practices of any third-party sites, products, or services.
-      </p>
-
-      <h3 style={{ color: '#2e7d32' }}>Remarketing</h3>
-      <p>
-        <a href="https://www.cerealswale.com/" style={{ color: '#2e7d32' }}>
-          https://www.cerealswale.com/
-        </a>{' '}
-        uses remarketing services to advertise on third-party websites
-        (including Google) to previous visitors to our site. It could mean that
-        we advertise to previous visitors who haven't completed a task on our
-        site, for example, using the contact form to make an inquiry. This could
-        be in the form of an advertisement on the Google search results page or a
-        site in the Google Display Network. Third-party vendors, including
-        Google, use cookies to serve ads based on someone's past visits. Any data
-        collected will be used in accordance with our own privacy policy and
-        Google's privacy policy.
-      </p>
-
-      <h3 style={{ color: '#2e7d32' }}>
-        Protection Of Certain Personally-Identifying Information
-      </h3>
-      <p>
-        <strong>CATALYST SERVICE PRIVATE LIMITED</strong> discloses potentially
-        personally-identifying and personally-identifying information only to
-        those of its employees, contractors, and affiliated organizations that
-        (i) need to know that information in order to process it on our behalf or
-        to provide services available at our website, and (ii) that have agreed
-        not to disclose it to others. Some of those employees, contractors, and
-        affiliated organizations may be located outside of your home country; by
-        using our website, you consent to the transfer of such information to
-        them.
-      </p>
-
-      <p>
-        We will not rent or sell potentially personally-identifying and
-        personally-identifying information to anyone. Other than to its
-        employees, contractors, and affiliated organizations, as described above,{' '}
-        <strong>CATALYST SERVICE PRIVATE LIMITED</strong> discloses potentially
-        personally-identifying and personally-identifying information only in
-        response to a subpoena, court order, or other governmental request, or
-        when we believe in good faith that disclosure is reasonably necessary to
-        protect the property or rights of the company, third parties, or the
-        public at large.
+        us. We strongly advise you to review the Privacy Policy and terms and 
+        conditions of every site you visit.
       </p>
 
       <h3 style={{ color: '#2e7d32' }}>Cookies</h3>
       <p>
         To enrich and perfect your online experience,{' '}
-        <strong>CATALYST SERVICE PRIVATE LIMITED</strong> uses "Cookies", similar
-        technologies and services provided by others to display personalized
-        content, appropriate advertising, and store your preferences on your
-        computer. A cookie is a string of information that a website stores on a
-        visitor's computer, and that the visitor's browser provides to the
-        website each time the visitor returns. We use cookies to help identify
-        and track visitors, their usage of{' '}
-        <a href="https://www.cerealswale.com/" style={{ color: '#2e7d32' }}>
-          https://www.cerealswale.com/
-        </a>
-        , and their website access preferences.
-      </p>
-
-      <h3 style={{ color: '#2e7d32' }}>Business Transfers</h3>
-      <p>
-        If <strong>CATALYST SERVICE PRIVATE LIMITED</strong>, or substantially
-        all of its assets, were acquired, or in the unlikely event that we go out
-        of business or enter bankruptcy, user information would be one of the
-        assets that is transferred or acquired by a third party. You acknowledge
-        that such transfers may occur, and that any acquirer may continue to use
-        your personal information as set forth in this policy.
-      </p>
-
-      <h3 style={{ color: '#2e7d32' }}>Location Data</h3>
-      <p>
-        We may use and store information about your location if you give us
-        permission to do so ("Location Data"). We use this data to provide
-        features of our Service, and to improve and customize our Service. You
-        can enable or disable location services at any time by way of your
-        device settings.
+        <strong>CATALYST SERVICE PRIVATE LIMITED</strong> uses "Cookies" to 
+        display personalized content and store your preferences on your computer.
       </p>
 
       <h3 style={{ color: '#2e7d32' }}>
         Privacy Excellence: Our Pledge to Privacy by Design
       </h3>
-      <p>
-        <strong>CATALYST SERVICE PRIVATE LIMITED</strong> is committed to the
-        principles of Privacy by Design, prioritizing privacy protections from
-        the outset of the development of our systems and products. We adhere to
-        the Seven Principles:
-      </p>
-
       <ol style={{ paddingLeft: '20px' }}>
         <li>
           <strong>Proactive not Reactive:</strong> We anticipate and prevent
@@ -273,7 +131,7 @@ export default function PrivacyPolicy() {
         </li>
         <li>
           <strong>Full Functionality:</strong> We provide services without
-          compromising privacy (Positive-Sum).
+          compromising privacy.
         </li>
         <li>
           <strong>End-to-End Security:</strong> Robust security measures protect
@@ -295,11 +153,8 @@ export default function PrivacyPolicy() {
       <p style={{ fontSize: '12px', color: '#888' }}>
         Although most changes are likely to be minor,{' '}
         <strong>CATALYST SERVICE PRIVATE LIMITED</strong> may change its Privacy
-        Policy from time to time at its sole discretion. We encourage visitors to
-        frequently check this page for any changes. Your continued use of this
-        site after any change will constitute your acceptance of such change.
+        Policy from time to time at its sole discretion.
       </p>
     </div>
   );
 }
-```
